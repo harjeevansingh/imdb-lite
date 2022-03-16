cce4491cebdaa66258777f9b5ebc269ee29a9630
Harjeevan Singh <harjeevansingh@Harjeevansingh.bbrouter>
2022-03-17 00:00:54 +0530

    Basic folder structure and routes ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route path="/" component={Home}/>
+            <Route path="/movie/:imdbID" component={MovieDetail}/>
+            <Route component={PageNotFound}/>
+          </Switch>
+          <Footer></Footer>
+        </Router>
     </div>
   );
 }
