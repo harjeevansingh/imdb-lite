292db16de0f4258848b7921f089bc960b6fa3599
harjeevansingh <harjeevansingh01@gmail.com>
2022-03-22 23:46:44 +0530

    Header and app.js initial styles added styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import user from "../../images/user.png";
+import { Link } from "react-router-dom";
+import './Header.scss';
 
 const header = () => {
-    return (
-        <div>
-            header
-        </div>
-    );
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="user-image" />
+      </div>
+    </div>
+  );
 };
 
 export default header;
