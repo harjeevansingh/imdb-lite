75dca16ba420b49bd8fadfc583dbfbd304f27aba
harjeevansingh <harjeevansingh01@gmail.com>
2022-04-05 01:39:54 +0530

    feat: updated home and movies components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import apiKey from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
-const home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const reponse = await movieApi
+        .get(`?apikey=${apiKey}&s=${movieText}&type=movie`)
+        .catch((error) => {
+          console.log("Error : ", error);
+        });
+      // console.log("The Response from API - ", reponse);
+      dispatch(addMovies(reponse.data));
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banned-img"></div>
+      <MovieListing></MovieListing>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { getAllMoveis } from "../../features/movies/movieSlice";
+import { useSelector } from "react-redux";
+import MovieCard from "../MovieCard/MovieCard";
+import './MovieListing.scss';
 
 const MovieListing = () => {
-    return (
-        <div>
-            MovieListing
-        </div>
+  const movies = useSelector(getAllMoveis);
+  console.log(movies);
+  let renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie}/>;
+      })
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.error}</h3>
+      </div>
     );
+  return (
+    <div className="movies-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
